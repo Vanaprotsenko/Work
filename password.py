cf2eee1f12ab082af4dc3b0b83a787002f9b5109
Vanaprotsenko <97354857+Vanaprotsenko@users.noreply.github.com>
2023-02-10 21:19:12 +0200

    Add files via upload

---
+++ password.py
@@ -0,0 +1,24 @@
+
+username=input("Username:")
+def pas():
+ password=input("Password:") 
+ password2=input("password2:") 
+ if password!=password2:
+  print("Error: Паролі не співпадають ")
+  return pas
+while True:
+ pas()
+
+   
+
+   
+ 
+     
+
+
+
+
+
+
+
+
