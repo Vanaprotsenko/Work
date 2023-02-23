1cd3e9879dd84fbd6198264909287d660d915396
Vanaprotsenko <97354857+Vanaprotsenko@users.noreply.github.com>
2023-02-23 12:54:30 +0200

    Update OOP1.py

--- OOP1.py
+++ OOP1.py
@@ -1,17 +1,25 @@
+from random import randint
 class Book:
-   def __init__(self,name,author):
+    def __init__(self,name,book):
       self.name = name
-      self.author = author
-   def spisok(self):
-      visitors=[
-         {
-            f"{self.author}:'Jame Jr'",
-            f"{self.name}:'The way to successfully'"
-         }
-      ]
-      return visitors
-book1=Book('The way to successfully','Jame Jr')
-book1.spisok()
-print(book1)
+      self.book= book
+    def __str__(self):
+        total=f"{self.name} узяв книгу {self.book} "
+        return total
+    def take_book(self,visitors):
+        print(f"{self.user} узяв книгу{self.name} автора{self.author}")
+        username=input("Username:")
+        book=input("Book:")
+        username['key'].append(visitors)
+        book ['key'].append(visitors)
+        visitors=[
+            f"{self.name}:'Jame'",
+            f"{self.book}:'The Way to succeed'",
+        ]
+        print(f"{self.name} взял книгу {self.book}")
+
+
+take1=Book()
+take1.take_book()
 
 
