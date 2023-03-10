daa6c63146c2216fdeabb1161598eeed7720a31f
Vanaprotsenko <97354857+Vanaprotsenko@users.noreply.github.com>
2023-03-10 10:13:19 +0200

    Update Pattern_2.py

--- Pattern_2.py
+++ Pattern_2.py
@@ -1,6 +1,6 @@
 import re
 number=input("Введіть ваші дані таким чином :Компанія:Агропром Власник:Адрій Капіталізація:3млн")
-pattern=r'^Компанія:[А-я]+.Власник:[А-я]+.Капіталізація:\d[А-я]+'
+pattern=r'^Компанія:[А-я]+'
 res=re.match(pattern,number)
 if res:
     print("Valid pattern")
